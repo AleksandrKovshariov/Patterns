1b9bf5aabc838bdb07b68b7a77e5e0ce2b3a5988
Aleksandr Kovsharov <sanforg2@gmail.com>
2019-05-15 07:26:07 +0300

    Added Bridge pattern

---
+++ src/main/java/bridge/Test.java
@@ -0,0 +1,12 @@
+package bridge;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Shape tri = new Triangle(new RedColor());
+        tri.applyColor();
+
+        Shape pent = new Pentagon(new GreenColor());
+        pent.applyColor();
+    }
+}
