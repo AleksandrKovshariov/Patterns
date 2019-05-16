8b50d7719123acbed55140398f3a7474c2f27e3e
Aleksandr Kovsharov <sanforg2@gmail.com>
2019-05-16 15:04:04 +0300

    Added Iterator pattern

---
+++ src/main/java/command/Test.java
@@ -0,0 +1,13 @@
+package command;
+
+public class Test {
+    public static void main(String[] args) {
+        TextFileOperationExecutor textFileOperationExecutor
+                = new TextFileOperationExecutor();
+
+        System.out.println(textFileOperationExecutor.executeOperation(
+                new OpenTextFileOperation(new TextFile("file1.txt"))));
+        System.out.println(textFileOperationExecutor.executeOperation(
+                new SaveTextFileOperation(new TextFile("file2.txt"))));
+    }
+}
